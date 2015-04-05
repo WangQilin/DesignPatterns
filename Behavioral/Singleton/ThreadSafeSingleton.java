74d953fac05a514ce7984999235bedbd3d070964
Wang Qilin <wangqilin129@gmail.com>
2015-04-05 20:33:01 +0800

    double-checked locking

--- Behavioral/Singleton/ThreadSafeSingleton.java
+++ Behavioral/Singleton/ThreadSafeSingleton.java
@@ -1,3 +1,4 @@
+// use synchronized keyword to make the implementation thread-safe
 public class ThreadSafeSingleton {
     private static ThreadSafeSingleton instance;
 
@@ -8,6 +9,28 @@ public class ThreadSafeSingleton {
     public static synchronized ThreadSafeSingleton getInstance() {
         if (instance == null) {
             instance = new ThreadSafeSingleton();
+        }
+
+        return instance;
+    }
+}
+
+
+// but synchronized implementation is too costly, below is the implementation which uses double-checked locking
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+
+    }
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
         }
 
         return instance;
